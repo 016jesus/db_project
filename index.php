975724dbd063ced94557a4b94f4fe955c159f340
DAMT777 <diegomachadotovar4@gmail.com>
2024-11-10 13:47:08 -0500

    prueba

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Consulta de Instituciones</title>
+    <title>Consulta</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
